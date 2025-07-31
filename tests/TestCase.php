e46d7ec35f391f4d30095f248fd15b0f4f351b0a
Wesley Silva <wesleysilvadb@hotmail.com>
2025-07-31 14:52:57 -0300

    feat: centralizing authentication token creation

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,23 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use RefreshDatabase;
+
+    protected string $token;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $user = User::factory()->create();
+
+        $this->token = Auth::fromUser($user);
+    } 
 }
