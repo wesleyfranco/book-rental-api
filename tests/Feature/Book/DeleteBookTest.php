2b8837486bcd598ec8cf39472ca032f9607835c3
Wesley Silva <wesleysilvadb@hotmail.com>
2025-08-01 12:55:11 -0300

    feat: adding a test for book deletion

---
+++ tests/Feature/Book/DeleteBookTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\Feature\Book;
+
+use Tests\TestCase;
+
+class DeleteBookTest extends TestCase
+{
+
+    private string $endpoint = 'api/v1/books';
+
+    public function test_check_if_the_book_has_been_deleted_successfully(): void
+    {
+        $response = $this->deleteJson(
+            "{$this->endpoint}/{$this->book->id}",
+            [],
+            [
+                'Authorization' => 'Bearer ' . $this->token,
+            ],
+        );
+
+        $response->assertStatus(200)
+            ->assertExactJsonStructure(
+            [
+                'success',
+                'message',
+            ],
+        );
+    }
+
+    public function test_check_that_the_book_is_not_found(): void
+    {
+        $response = $this->deleteJson(
+            "{$this->endpoint}/abc",
+            [],
+            [
+                'Authorization' => 'Bearer ' . $this->token,
+            ],
+        );
+
+        $response->assertStatus(404)
+            ->assertExactJsonStructure(
+            [
+                'success',
+                'message',
+            ],
+        );
+    }
+
+    public function test_checks_if_the_route_is_protected(): void
+    {
+        $token = 'abc';
+
+        $response = $this->deleteJson(
+            "{$this->endpoint}/{$this->book->id}",
+            [],
+            [
+                'Authorization' => 'Bearer ' . $token,
+            ],
+        );
+
+        $response->assertStatus(401)
+            ->assertExactJsonStructure(
+            [
+                'success',
+                'message',
+            ],
+        );
+    }
+}
